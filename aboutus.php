fa1cb1a2213713ea4ea826a6f61dbcaa5c316f5b
CianWalters <61118431+CianWalters@users.noreply.github.com>
2022-12-02 04:51:19 -0500

    Added contact buttons

--- aboutus.php
+++ aboutus.php
@@ -7,7 +7,7 @@ require_once 'includes/header.php';
 
 <h1 class="text-center" style="font-family:algerian"><em>ABOUT US</em></h1>
 
-<p>
+<h6>
     <em>
 Express Fitness is a locally owned and operated fitness club, 
 which focuses on providing a first world fitness facility at a price you can afford. 
@@ -16,7 +16,7 @@ Express Fitness is open 24 hours a day for your convenience,
 so you can work-out at a time that suits you! Any time, day or night. 
 Only the best and safest equipment will be used in the Express Fitness chain of clubs under the supervision of a professional team.
 </em>
-</p>
+</h6>
 
 
 
--- contacts.php
+++ contacts.php
@@ -8,18 +8,20 @@ require_once 'includes/header.php';
 <h1 class="text-center" style="font-family:algerian"><em>CONTACTS</em></h1>
 
 
-<p align="center">
+<h6 align="center">
 Contact us at:<br>
 Telephone: 876-962-1535<br>
 WhatsApp: 876-334-2180<br>
-</p>
+</h6>
 
-<p align="center">
-Follow us on:<br>
-<a href="https://www.instagram.com/">Instagram: wally__bee</a> <br>
-<a href="https://twitter.com/">Twitter: @fitness_wallybee</a> <br>
-YouTube: Wally Bee Fitness<br>
-</p>
+<h6 align="center">
+Follow us on:<br><br>
+<a href="https://www.instagram.com/wally__bee/" target="_blank" class="btn btn-outline-danger">Instagram: wally__bee</a> <br><br>
+<a href="https://twitter.com/" target="_blank" class="btn btn-outline-info">Twitter: @fitness_wallybee</a> <br><br>
+<a href="https://www.youtube.com/channel/UCxq89YNNlZOxQWqUOoBSbVw" target="_blank" class="btn btn-danger">YouTube: @fitness_wallybee</a> <br><br>
+
+
+</h6>
 
 <br>
 <br>
@@ -28,4 +30,6 @@ YouTube: Wally Bee Fitness<br>
 <br>
 
 <?php require_once 'includes/footer.php'; ?>
+
+<!-- <h6> used instead of <p> for readability  -->
 
